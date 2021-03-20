7a304d93a4345bf5c3db51c0e51ee5b4e0d4da5d
Matheus da Costa da Silva <matheuscosta.tech@gmail.com>
2021-03-20 10:10:39 -0300

    Deliting the api root view, because the DefaultRouter which we're using automatically creates the API root

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics, permissions, renderers, viewsets
-from rest_framework.decorators import api_view, action
+from rest_framework.decorators import  action
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 
@@ -8,14 +8,6 @@ from snippets.serializers import SnippetSerializer, UserSerializer
 
 from django.contrib.auth.models import User
 from snippets.permissions import IsOwnerOrReadOnly
-
-
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'snippets': reverse('snippet-list', request=request, format=format)
-    })
 
 
 class SnippetViewSet(viewsets.ModelViewSet):

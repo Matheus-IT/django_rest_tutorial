64d12a0c43f9a1715fe18ca2790fae52692e229d
Matheus da Costa da Silva <matheuscosta.tech@gmail.com>
2021-03-20 08:50:20 -0300

    Changing the user views to viewsets

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, permissions, renderers
+from rest_framework import generics, permissions, renderers, viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
@@ -48,11 +48,9 @@ class SnippetHighlight(generics.GenericAPIView):
         return Response(snippet.highlighted)
 
 
-class UserList(generics.ListAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-class UserDetail(generics.RetrieveAPIView):
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    This viewset automatically provides `list` and `retrieve` actions.
+    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
